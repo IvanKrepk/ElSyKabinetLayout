154862f85778ded7e563cbd08fde882eb0f0905e
IvanKrepk <i.am.krepkov.ivan@yandex.ru>
2025-06-16 14:42:24 +0300

    Add DateRange

--- assets/js/script.js
+++ assets/js/script.js
@@ -34,11 +34,13 @@ document.addEventListener("DOMContentLoaded", () => {
         //#region focus
 
         title.addEventListener("focus", () => {
+            title.style.boxShadow = "0px 0px 4px 1px rgba(34, 60, 80, 0.15)";
             title.style.borderColor = "var(--color-border-focus)";
             list.style.borderColor = "var(--color-border-focus)";
         });
 
         title.addEventListener("blur", () => {
+            title.style.boxShadow = "none";
             title.style.borderColor = "var(--color-border)";
             list.style.borderColor = "var(--color-border)";
 
@@ -80,4 +82,70 @@ document.addEventListener("DOMContentLoaded", () => {
 
     //#endregion
 
+
+    //#region "date-period" class
+
+    let datePeriods = document.querySelectorAll('.date-period');
+    datePeriods.forEach(datePeriod => {
+
+        let datePeriodInput = datePeriod.querySelector('.date-period-input');
+
+        $(datePeriodInput).daterangepicker({
+            startDate: moment().subtract(20, 'days'),
+            endDate: moment(),
+            autoUpdateInput: false,
+            showCustomRangeLabel: false,
+            startOfWeek: "monday",
+            locale: {
+                "format": "DD.MM.YYYY",
+                "separator": " - ",
+                "applyLabel": "Ок",
+                "cancelLabel": "Назад",
+                "daysOfWeek": [
+                    "Вс",
+                    "Пн",
+                    "Вт",
+                    "Ср",
+                    "Чт",
+                    "Пт",
+                    "Сб"
+                ],
+                "monthNames": [
+                    "Январь",
+                    "Февраль",
+                    "Март",
+                    "Апрель",
+                    "Май",
+                    "Июнь",
+                    "Июль",
+                    "Август",
+                    "Сентябрь",
+                    "Октябрь",
+                    "Ноябрь",
+                    "Декабрь"
+                ]
+            }
+        });
+
+        let valueDateRange = datePeriodInput.querySelector(".date-period-value");
+        let dateRangePicker = $(datePeriodInput).data('daterangepicker');
+        let btnRefresh = datePeriod.querySelector('.date-period-refresh');
+
+        dateRangePicker.callback = function() {
+            let startDate = dateRangePicker.startDate.format("YYYY/MM/DD");
+            let endDate = dateRangePicker.endDate.format("YYYY/MM/DD");
+            valueDateRange.textContent = `${startDate}-${endDate}`;
+            valueDateRange.style.color = "var(--color-secondary)";
+            btnRefresh.style.fill = "var(--color-secondary)";
+        }
+
+        btnRefresh.addEventListener('click', () => {
+            valueDateRange.textContent = "Введите период...";
+            valueDateRange.style.color = "var(--color-inactive)";
+            btnRefresh.style.fill = "var(--color-inactive)";
+        });
+
+    });
+
+    //#endregion
 });
